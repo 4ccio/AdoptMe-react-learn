347ce6ed43c173d659a5516ce89068516de74dcb
Mikhail Rodionov <misha25025.su@gmail.com>
2024-10-07 23:41:13 +0300

    edited: from remade to uncontrolled with react query to search for pets

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,41 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import useBreedList from "./useBreedList";
+import fetSearch from "./fecthSearch";
 import Results from "./Results";
 import SearchFrom from "./SearchForm";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [pets, setPets] = useState([]);
-  const [location, setLocation] = useState("");
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
   const [animal, setAnimal] = useState("");
-  const [breed, setBreed] = useState("");
   const [breeds] = useBreedList(animal);
 
-  useEffect(() => {
-    requestPets();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  async function requestPets() {
-    const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-    );
-    const json = await res.json();
-
-    setPets(json.pets);
-  }
+  const results = useQuery(["search", requestParams], fetSearch);
+  const pets = results?.data?.pets ?? [];
 
   return (
     <div className="search-params">
       <SearchFrom
         animals={ANIMALS}
-        location={location}
-        setLocation={setLocation}
         animal={animal}
         setAnimal={setAnimal}
-        breed={breed}
-        setBreed={setBreed}
         breeds={breeds}
-        requestPets={requestPets}
+        setRequestParams={setRequestParams}
       ></SearchFrom>
       <Results pets={pets}></Results>
     </div>
