a6be185a4243055c72857f184a8cfa9950468ef4
Mikhail Rodionov <misha25025.su@gmail.com>
2024-10-07 16:49:09 +0300

    added: react query, fetchPets to load and cache details info about pet

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <h1>Adopt Me!</h1>
-      <Routes>
-        <Route path="/details/:id" element={<Details />}></Route>
-        <Route path="/" element={<SearchParams></SearchParams>}></Route>
-      </Routes>
-      {/* <SearchParams></SearchParams> */}
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams></SearchParams>}></Route>
+          </Routes>
+        </QueryClientProvider>
+        {/* <SearchParams></SearchParams> */}
+      </BrowserRouter>
+    </div>
   );
 };
 
